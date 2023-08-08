ed4506e0c5115f7db0e49b8e9132723abf032b92
1-Hee <onehee9710@gmail.com>
2023-08-08 09:57:10 +0900

    comp : 아티스트 검색 페이지 생성 및 data class 위치 변경

---
+++ wea/app/src/main/java/com/artist/wea/data/ArtistInfo.kt
@@ -0,0 +1,8 @@
+package com.artist.wea.data
+
+data class ArtistInfo(
+    val profileImgURL:String = "",
+    val artistName:String = "",
+    val location:String = "",
+    val comment:String = ""
+)
