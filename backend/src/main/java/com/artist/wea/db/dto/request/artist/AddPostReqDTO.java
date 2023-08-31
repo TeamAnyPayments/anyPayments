defce57035db15c4dc07273f6fa806c3dd19129c
esovo <cho50321@naver.com>
2023-08-31 10:21:00 +0900

    feat: 회원가입, 로그인 DTO 어노테이션 추가

--- backend/src/main/java/com/artist/wea/db/dto/request/artist/AddPostReqDTO.java
+++ backend/src/main/java/com/artist/wea/db/dto/request/artist/AddPostReqDTO.java
@@ -2,11 +2,13 @@ package com.artist.wea.db.dto.request.artist;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AddPostReqDTO {
 
     private String name;
