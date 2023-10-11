b3ceb5ebcf9b2b1d2e851dfc3768dcb6e899b0a2
1-hee <onehee9710@gmail.com>
2023-10-11 23:21:34 +0900

    feat : toss api 개발자 계정과 키 등록 완료 및 로깅 추가

--- wea/app/build.gradle.kts
+++ wea/app/build.gradle.kts
@@ -1,7 +1,5 @@
 
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 
 plugins {
     id("com.android.application")
@@ -24,29 +22,36 @@ android {
             useSupportLibrary = true
         }
 
-        manifestPlaceholders["NAVER_CLIENT_ID"] =  "NAVER_CLIENT_ID"
+        manifestPlaceholders["NAVER_CLIENT_ID"] = "NAVER_CLIENT_ID"
         buildConfigField("String", "NAVER_CLIENT_ID", getApiKey("NAVER_CLIENT_ID"))
 
-        android.applicationVariants.all { variant ->
-            // 릴리즈 apk 파일을 생성할 때 파일명에 규칙을 주도록 하자.
-            if (variant.buildType.name == "release" || variant.buildType.name == "debug") {
-                variant.outputs.all { output ->
-                    val date = LocalDateTime.now()
-                    val formattedDate = SimpleDateFormat("yyyy_MM_dd").format(date)
+        manifestPlaceholders["TOSS_CLIENT_KEY"] = "TOSS_CLIENT_KEY"
+        buildConfigField("String", "TOSS_CLIENT_KEY", getApiKey("TOSS_CLIENT_KEY"))
 
-                    // apk 파일명에 규칙 주기
-                    if (output.outputFile != null && output.outputFile.name.endsWith(".aab")) {
-                        val appPrefix = "wea"
-                        val versionName = variant.versionName
-                        val buildType = variant.buildType.name
-                        val alignedFileName = "${appPrefix}_${buildType}_${formattedDate}-${versionName}.aab"
-                        output.outputFile.renameTo(File(alignedFileName))
-                    }
-                    true
-                }
-            }
-            variant.buildType.name == "release" || variant.buildType.name == "debug"
-        }
+        manifestPlaceholders["TOSS_SECRET_KEY"] = "TOSS_SECRET_KEY"
+        buildConfigField("String", "TOSS_SECRET_KEY", getApiKey("TOSS_SECRET_KEY"))
+
+
+//        android.applicationVariants.all { variant ->
+//            // 릴리즈 apk 파일을 생성할 때 파일명에 규칙을 주도록 하자.
+//            if (variant.buildType.name == "release" || variant.buildType.name == "debug") {
+//                variant.outputs.all { output ->
+//                    val date = LocalDateTime.now()
+//                    val formattedDate = SimpleDateFormat("yyyy_MM_dd").format(date)
+//
+//                    // apk 파일명에 규칙 주기
+//                    if (output.outputFile != null && output.outputFile.name.endsWith(".aab")) {
+//                        val appPrefix = "wea"
+//                        val versionName = variant.versionName
+//                        val buildType = variant.buildType.name
+//                        val alignedFileName = "${appPrefix}_${buildType}_${formattedDate}-${versionName}.aab"
+//                        output.outputFile.renameTo(File(alignedFileName))
+//                    }
+//                    true
+//                }
+//            }
+//            variant.buildType.name == "release" || variant.buildType.name == "debug"
+//        }
     }
 
 
@@ -54,6 +59,8 @@ android {
 
         debug {
             buildConfigField("String", "NAVER_CLIENT_ID", getApiKey("NAVER_CLIENT_ID"))
+            buildConfigField("String", "TOSS_CLIENT_KEY", getApiKey("TOSS_CLIENT_KEY"))
+            buildConfigField("String", "TOSS_SECRET_KEY", getApiKey("TOSS_SECRET_KEY"))
         }
         release {
             isMinifyEnabled = false
@@ -62,6 +69,8 @@ android {
                 "proguard-rules.pro"
             )
             buildConfigField("String", "NAVER_CLIENT_ID", getApiKey("NAVER_CLIENT_ID"))
+            buildConfigField("String", "TOSS_CLIENT_KEY", getApiKey("TOSS_CLIENT_KEY"))
+            buildConfigField("String", "TOSS_SECRET_KEY", getApiKey("TOSS_SECRET_KEY"))
         }
     }
     compileOptions {
--- wea/app/src/main/java/com/artist/wea/activity/PaymentsActivity.kt
+++ wea/app/src/main/java/com/artist/wea/activity/PaymentsActivity.kt
@@ -2,8 +2,13 @@ package com.artist.wea.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.artist.wea.BuildConfig
+import com.artist.wea.R
+import com.artist.wea.data.TossPaySuccess
 import com.artist.wea.databinding.ActivityPaymentsBinding
+import com.artist.wea.util.ToastManager
 import com.tosspayments.paymentsdk.PaymentWidget
 import com.tosspayments.paymentsdk.model.PaymentCallback
 import com.tosspayments.paymentsdk.model.PaymentWidgetStatusListener
@@ -18,14 +23,27 @@ class PaymentsActivity : AppCompatActivity() {
 
     // 뷰바인딩을 적용하였으므로 XXXBiding 클래스를 통해 뷰바인딩을 할 예정
     val binding by lazy {ActivityPaymentsBinding.inflate(layoutInflater)}
+    var paymentsState:Boolean = false;
+
+    // 임시로 액티비티 內 전역 변수로 결제 결과 표시
+    var tossPaySuccess: TossPaySuccess? = null;
+
+    // 결제 금액 (임시)
+    val payAmount:Number = 1000;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // 초기 버튼에는 ... 표시를 해서 결제가 진행중이지 않음을 표시
+        binding.payButton.text = ". . .";
+        paymentsState = false;
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         // 아래의 코드들은 토스 개발자 사이트에서 제공한 샘플 코드임
-        val clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq"
-        val secretKey = "test_sk_zXLkKEypNArWmo50nX3lmeaxYG5R"
+        val clientKey = BuildConfig.TOSS_CLIENT_KEY
+        val secretKey = BuildConfig.TOSS_SECRET_KEY
 
         // 페이먼츠 위젯에 필요한 기본 속성에 대해 세팅해준다!
         val paymentWidget = PaymentWidget(
@@ -39,6 +57,8 @@ class PaymentsActivity : AppCompatActivity() {
             override fun onLoad() {
                 val message = "결제위젯 렌더링 완료"
                 Log.d("PaymentWidgetStatusListener", message)
+                paymentsState = true;
+                binding.payButton.text = "결제하기"
             }
         }
 
@@ -47,7 +67,7 @@ class PaymentsActivity : AppCompatActivity() {
         paymentWidget.run {
             renderPaymentMethods(
                 method = binding.paymentWidget,
-                amount = PaymentMethod.Rendering.Amount(10000),
+                amount = PaymentMethod.Rendering.Amount(payAmount),
                 paymentWidgetStatusListener = paymentMethodWidgetStatusListener
             )
 
@@ -57,21 +77,48 @@ class PaymentsActivity : AppCompatActivity() {
         // 결제버튼을 누르면 다음 단계로 넘어가게 하는 로직.
         // 이 부분은 토스가 제공하지 않으므로 스스로 xml에 버튼 추가해서 이벤트 리스너 셋팅해줘야 한다.
         binding.payButton.setOnClickListener {
-            paymentWidget.requestPayment(
-                paymentInfo = PaymentMethod.PaymentInfo(orderId = "wBWO9RJXO0UYqJMV4er8J", orderName = "wea"),
-                paymentCallback = object : PaymentCallback {
-                    // 결제 프로세스에 대한 콜백 함수이다.
-                    override fun onPaymentSuccess(success: TossPaymentResult.Success) {
-                        Log.i("success:::", success.paymentKey)
-                        Log.i("success:::", success.orderId)
-                        Log.i("success:::", success.amount.toString())
-                    }
+            if(paymentsState){ // 렌더링 완료시에면 결제 프로세스가 가능하도록!
+                paymentWidget.requestPayment(
+                    paymentInfo = PaymentMethod.PaymentInfo(orderId = "wBWO9RJXO0UYqJMV4er8J", orderName = "wea"),
+                    paymentCallback = object : PaymentCallback {
+                        // 결제 프로세스에 대한 콜백 함수이다.
+                        override fun onPaymentSuccess(success: TossPaymentResult.Success) {
+                            Log.i("success:::", success.paymentKey)
+                            Log.i("success:::", success.orderId)
+                            Log.i("success:::", success.amount.toString())
 
-                    override fun onPaymentFailed(fail: TossPaymentResult.Fail) {
-                        Log.e("fail:::",fail.errorMessage)
+                            // 결과를 TossPaySuccess 객체에 저장
+                            tossPaySuccess?.paymentKey ?: success.paymentKey;
+                            tossPaySuccess?.orderId ?: success.orderId;
+                            tossPaySuccess?.amount ?: success.amount;
+
+                            ToastManager.shortToast(
+                                this@PaymentsActivity,
+                                getString(R.string.txt_pay_complete)
+                            )
+                        }
+
+                        override fun onPaymentFailed(fail: TossPaymentResult.Fail) {
+                            Log.e("fail:::",fail.errorMessage)
+                            ToastManager.shortToast(
+                                this@PaymentsActivity,
+                                getString(R.string.txt_pay_fail)
+                            )
+                        }
                     }
-                }
-            )
+                )
+            }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // 뒤로 가기 동작을 수행하거나 원하는 작업을 수행하세요.
+                onBackPressed() // deprecated 되지 않은 메서드로 교체..
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
